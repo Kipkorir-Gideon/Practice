2876b8814cf6f1664dda0e0108a79655c140ea68
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-12 15:40:27 +0300

    Change a class to id for tasks.

--- javascript/CRUD/To-Do/main.js
+++ javascript/CRUD/To-Do/main.js
@@ -48,22 +48,22 @@ let acceptData = () => {
 let createTasks = () => {
     tasks.innerHTML = "";
     data.map((x, y) => {
-        return (tasks.innerHTML += `
-        <div id=${y}>
+      return (tasks.innerHTML += `
+      <div id=${y}>
             <span class="fw-bold">${x.text}</span>
             <span class="small text-secondary">${x.date}</span>
             <p>${x.description}</p>
-
+    
             <span class="options">
-                <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
-                <i onClick="deleteTask(this);createTasks()" class="fasfa-trash-alt"></i>
+              <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+              <i onClick ="deleteTask(this);createTasks()" class="fas fa-trash-alt"></i>
             </span>
-        </div>
-        `);
+          </div>
+      `);
     });
-
+  
     resetForm();
-};
+  };
 
 let resetForm = () => {
     textInput.value = "";
