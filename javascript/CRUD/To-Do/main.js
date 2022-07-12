ac85c948ea1d424642f86829c6796178d3855f00
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-12 15:06:51 +0300

    Add a function for creating a new task.

--- javascript/CRUD/To-Do/main.js
+++ javascript/CRUD/To-Do/main.js
@@ -42,3 +42,21 @@ let acceptData = () => {
 
     console.log(data);
 }
+
+let createTasks = () => {
+    tasks.innerHTML = "";
+    data.map((x, y) => {
+        return (tasks.innerHTML += `
+        <div id=${y}>
+            <span class="fw-bold">${x.text}</span>
+            <span class="small text-secondary">${x.date}</span>
+            <p>${x.description}</p>
+
+            <span class="options">
+                <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fas fa-edit"></i>
+                <i onClick="deleteTask(this);createTasks()" class="fasfa-trash-alt"></i>
+            </span>
+        </div>
+        `);
+    });
+};
