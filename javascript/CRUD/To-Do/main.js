f344086ae1107fee4f1637e100a25f4c0ff42ac9
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-12 14:08:03 +0300

    Add a function to prevent submitting blank input fields.

--- javascript/CRUD/To-Do/main.js
+++ javascript/CRUD/To-Do/main.js
@@ -10,3 +10,13 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     formValidation();
 });
+
+let formValidation = () => {
+    if ( textInput.value === "") {
+        console.log("failure");
+        msg.innerHTML = "Task cannot be blank";
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+    }
+};
