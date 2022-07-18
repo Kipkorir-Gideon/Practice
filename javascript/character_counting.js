3125aea2519e77b9a5d9c0e7b73950f9aba0f67e
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-18 12:10:25 +0300

    Add a slice method to only show the first 140 characters.

--- javascript/character_counting.js
+++ javascript/character_counting.js
@@ -1,3 +1,5 @@
 var tweet = prompt("Compose your tweet:");
 var tweetCount = tweet.length;
+var tweetUnder140 = tweet.slice(0,140);
 alert("You have written " + tweetCount + " characters, you have " + (140 - tweetCount) + " characters remaining.");
+alert(tweetUnder140);
