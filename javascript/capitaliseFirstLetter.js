7e67207d8dd40c8b9d3578f1a3f4672abf057d6c
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-20 12:53:55 +0300

    How to capitalise the first character in a string.

---
+++ javascript/capitaliseFirstLetter.js
@@ -0,0 +1,11 @@
+var name = prompt("Enter your name: ");
+
+var firstChar = name.slice(0, 1);
+
+var capitaliseFirstChar = firstChar.toUpperCase();
+
+var restOfChar = name.slice(1, name.length);
+
+var capitaliseName = capitaliseFirstChar + restOfChar;
+
+alert("Hello, " + capitaliseName);
