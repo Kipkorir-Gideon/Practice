f3f14cb4199741182124928be876875386ec243b
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-09-20 16:36:19 +0300

    Practiced on for loops.

--- Python/Practice/input.py
+++ Python/Practice/input.py
@@ -386,13 +386,13 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
-for char in range(1, nr_letters + 1):
+for char in range(0, nr_letters):
     password_list.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
+for char in range(0, nr_symbols):
     password_list.append(random.choice(symbols))
 
-for char in range(1, nr_numbers + 1):
+for char in range(0, nr_numbers):
     password_list.append(random.choice(numbers))
 random.shuffle(password_list)
 
