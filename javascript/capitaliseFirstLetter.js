fe9b378812903ab5672ed0e54e39ec302ff57810
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-09-23 12:21:58 +0300

    I can't remember what I did.

--- javascript/capitaliseFirstLetter.js
+++ javascript/capitaliseFirstLetter.js
@@ -5,6 +5,7 @@ var firstChar = name.slice(0, 1);
 var capitaliseFirstChar = firstChar.toUpperCase();
 
 var restOfChar = name.slice(1, name.length);
+restOfChar = restOfChar.toLowerCase();
 
 var capitaliseName = capitaliseFirstChar + restOfChar;
 
